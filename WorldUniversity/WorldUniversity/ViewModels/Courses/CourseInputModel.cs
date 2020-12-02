0c59196d1f8268726bf72e56df426fda19a5dd62
shaddowmonster11 <74061027+shaddowmonster11@users.noreply.github.com>
2020-12-02 18:19:13 +0200

    migration changes

---
+++ WorldUniversity/WorldUniversity/ViewModels/Courses/CourseInputModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WorldUniversity.ViewModels.Courses
+{
+    public class CourseInputModel
+    {
+        [StringLength(50, MinimumLength = 1)]
+        public string Title { get; set; }
+
+        [Range(0, 10)]
+        public int Credits { get; set; }
+
+        public int DepartmentId { get; set; }
+    }
+}
