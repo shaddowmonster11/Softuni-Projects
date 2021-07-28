f77ca8407ae95d87a21bf0e9cbbb10ee2fdfe23d
shaddowmonster11 <74061027+shaddowmonster11@users.noreply.github.com>
2021-07-28 20:58:49 +0300

    Working on ExamLayout(navbar fixed)

--- WorldUniversity/WorldUniversity/Controllers/ExamSessionsController.cs
+++ WorldUniversity/WorldUniversity/Controllers/ExamSessionsController.cs
@@ -3,15 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WorldUniversity.Services.Exams;
+using WorldUniversity.ViewModels.Questions;
 
 namespace WorldUniversity.Controllers
 {
     public class ExamSessionsController:Controller
     {
+        private readonly IExamsService examsService;
+        private readonly IQuestionsService questionsService;
+
+        public ExamSessionsController(IExamsService examsService
+            , IQuestionsService questionsService)
+        {
+            this.examsService = examsService;
+            this.questionsService = questionsService;
+        }
         public IActionResult Index()
         {
-           
-            return View();
+            var exams = examsService.GetAllExams();
+            return View(exams);
         }
     }
 }
