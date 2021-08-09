e860bac96fbf6272a5ad310fe5b5a570465e8d3d
shaddowmonster11 <74061027+shaddowmonster11@users.noreply.github.com>
2021-08-09 20:29:29 +0300

    Working on Exam Apllication and Take Exam

--- WorldUniversity/WorldUniversity/Controllers/ExamSessionsController.cs
+++ WorldUniversity/WorldUniversity/Controllers/ExamSessionsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,9 +21,39 @@ namespace WorldUniversity.Controllers
             this.questionsService = questionsService;
         }
         public IActionResult Index()
+        {           
+            return View();
+        }
+        public IActionResult ListExams()
         {
             var exams = examsService.GetAllExams();
             return View(exams);
         }
+        public IActionResult TakeExam()
+        { 
+            return View();
+        }
+       /* [HttpPost, ActionName("TakeExam")]
+        [ValidateAntiForgeryToken]
+        public IActionResult TakeConfirmation(int id)
+        {
+            var student = 0;
+
+            if (student == null)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            try
+            {
+               // await this.studentService.DeleteStudent(id);
+                return RedirectToAction(nameof(Index));
+
+            }
+            catch (DbUpdateException)
+            {
+                return RedirectToAction(nameof(TakeExam), new { id = id, saveChangesError = true });
+            }
+        }*/
     }
 }
