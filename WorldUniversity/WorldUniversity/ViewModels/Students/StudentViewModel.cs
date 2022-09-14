f83b2c57566d6b7095730d7670eb030f1d893470
shaddowmonster11 <shaddowmonster11@gmail.com>
2022-09-14 18:12:00 +0300

    Adding Name Attributes to StudentViewModel

--- WorldUniversity/WorldUniversity/ViewModels/Students/StudentViewModel.cs
+++ WorldUniversity/WorldUniversity/ViewModels/Students/StudentViewModel.cs
@@ -8,7 +8,9 @@ namespace WorldUniversity.ViewModels.Students
     public class StudentViewModel
     {
         public string Id { get; set; }
+        [Display(Name = "First Name")]
         public string FirstName { get; set; }
+        [Display(Name = "Last Name")]
         public string LastName { get; set; }
         public string Email { get; set; }
         public string FullName
