58fb26ae7d629b0a8facdbd92fa2df83037834d6
shaddowmonster11 <shaddowmonster11@gmail.com>
2022-09-16 14:45:04 +0300

    setting the date of deletion of student/users

--- WorldUniversity/WorldUniversity/Services/StudentsService.cs
+++ WorldUniversity/WorldUniversity/Services/StudentsService.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using WorldUniversity.Data;
@@ -22,6 +23,7 @@ namespace WorldUniversity.Services
             student.IsDeleted = true;
             student.UserName = null;
             student.NormalizedUserName = null;
+            student.DeletedOn = DateTime.UtcNow;
             _context.Update(student);
             var studentEnrollemnts = _context.Enrollments.Where(s => s.StudentId == id).ToList();
             var roles = student.Roles.ToList();
@@ -30,17 +32,18 @@ namespace WorldUniversity.Services
                 roles.Remove(role);
                 _context.Update(role);
             }
-        
+
             if (studentEnrollemnts.Count != 0)
             {
                 foreach (var enrollement in studentEnrollemnts)
                 {
                     enrollement.IsDeleted = true;
+                    enrollement.DeletedOn = DateTime.UtcNow;
                     _context.Update(enrollement);
                 }
-                
+
             }
-          
+
             await _context.SaveChangesAsync();
         }
         public IQueryable<StudentViewModel> GetStudentAllData()
