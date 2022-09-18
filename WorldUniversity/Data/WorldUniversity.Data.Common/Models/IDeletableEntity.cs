af55173fbdcce403927b2cab118fc2cc9134ebc9
shaddowmonster11 <shaddowmonster11@gmail.com>
2022-09-18 16:18:59 +0300

    Create WorldUniversity.Data.Common and moving Entities there

---
+++ WorldUniversity/Data/WorldUniversity.Data.Common/Models/IDeletableEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WorldUniversity.Data.Common.Models
+{
+    public interface IDeletableEntity
+    {
+        bool IsDeleted { get; set; }
+        DateTime? DeletedOn { get; set; }
+    }
+}
