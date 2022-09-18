d6e2d334f4783944dd741e3af51fafd135b0e4a7
shaddowmonster11 <shaddowmonster11@gmail.com>
2022-09-18 16:14:37 +0300

    Creating WorldUniversity.Data project and moving Migrations/Repositories/DbContext and db seeders

---
+++ WorldUniversity/Data/WorldUniversity.Data/ApplicationDbContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using WorldUniversity.Data.Common.Models;
+using WorldUniversity.Data.Models;
+using WorldUniversity.Data.Models.ExamModels;
+
+namespace WorldUniversity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        private static readonly MethodInfo SetIsDeletedQueryFilterMethod =
+                   typeof(ApplicationDbContext).GetMethod(
+                       nameof(SetIsDeletedQueryFilter),
+                       BindingFlags.NonPublic | BindingFlags.Static);
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
+        public DbSet<CourseAssignment> CourseAssignments { get; set; }
+        public DbSet<ExamAssignment> ExamAssignments { get; set; }
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<ContactForm> ContactForm { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().ToTable("Courses");
+            modelBuilder.Entity<Enrollment>().ToTable("Enrollments");
+            modelBuilder.Entity<Department>().ToTable("Departments");
+            modelBuilder.Entity<Instructor>().ToTable("Instructors");
+            modelBuilder.Entity<OfficeAssignment>().ToTable("OfficeAssignments");
+            modelBuilder.Entity<CourseAssignment>().ToTable("CourseAssignments");
+            modelBuilder.Entity<ExamAssignment>().ToTable("ExamAssignments");
+            modelBuilder.Entity<Question>().ToTable("Questions");
+            modelBuilder.Entity<ContactForm>().ToTable("ContractForms");
+            modelBuilder.Entity<Exam>().ToTable("Exams");
+            modelBuilder.Entity<CourseAssignment>()
+                  .HasKey(c => new { c.CourseId, c.InstructorId });
+            modelBuilder.Entity<ExamAssignment>()
+                 .HasMany(p => p.Students)
+                 .WithMany(p => p.ExamAssignments);
+
+            base.OnModelCreating(modelBuilder);
+            EntityIndexesConfiguration.Configure(modelBuilder);
+            var entityTypes = modelBuilder.Model.GetEntityTypes().ToList();
+
+            // Set global query filter for not deleted entities only
+            var deletableEntityTypes = entityTypes
+                .Where(et => et.ClrType != null && typeof(IDeletableEntity).IsAssignableFrom(et.ClrType));
+            foreach (var deletableEntityType in deletableEntityTypes)
+            {
+                var method = SetIsDeletedQueryFilterMethod.MakeGenericMethod(deletableEntityType.ClrType);
+                method.Invoke(null, new object[] { modelBuilder });
+            }
+            foreach (var foreignKey in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                foreignKey.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+        }
+        private static void SetIsDeletedQueryFilter<T>(ModelBuilder builder)
+           where T : class, IDeletableEntity
+        {
+            builder.Entity<T>().HasQueryFilter(e => !e.IsDeleted);
+        }
+
+    }
+}
---
+++ WorldUniversity/Data/WorldUniversity.Data/IdentityDataInitializer.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Identity;
+using WorldUniversity.Data.Models;
+
+namespace WorldUniversity.Data
+{
+    public static class IdentityDataInitializer
+    {
+        public static void SeedData(UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        public static void SeedUsers(UserManager<ApplicationUser> userManager)
+        {
+            if (userManager.FindByNameAsync("user@wumail.bg").Result == null)
+            {
+                var user = new ApplicationUser();
+                user.FirstName = "Ivan";
+                user.LastName = "Ivanov";
+                user.UserName = "user@wumail.bg";
+                user.Email = "user@wumail.bg";
+                user.EmailConfirmed = true;
+                var result = userManager.CreateAsync(user, "12345678").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "User").Wait();
+                }
+            }
+
+            if (userManager.FindByNameAsync("admin@wumail.bg").Result == null)
+            {
+                var user = new ApplicationUser();
+                user.FirstName = "Ivan";
+                user.LastName = "Ivanov";
+                user.UserName = "admin@wumail.bg";
+                user.Email = "admin@wumail.bg";
+                user.EmailConfirmed = true;
+                var result = userManager.CreateAsync(user, "12345678").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Admin").Wait();
+                }
+            }
+
+            if (userManager.FindByNameAsync("mod@wumail.bg").Result == null)
+            {
+                var user = new ApplicationUser();
+                user.FirstName = "Hasan";
+                user.LastName = "Hasan123";
+                user.UserName = "mod@wumail.bg";
+                user.Email = "mod@wumail.bg";
+                user.EmailConfirmed = true;
+                var result = userManager.CreateAsync(user, "12345678").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Moderator").Wait();
+                }
+            }
+        }
+        public static void SeedRoles(RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("User").Result)
+            {
+                var role = new IdentityRole();
+                role.Name = "User";
+                role.NormalizedName = "Perform normal operations.";
+                var roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                role.NormalizedName = "Perform all the operations.";
+                var roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+            if (!roleManager.RoleExistsAsync("Moderator").Result)
+            {
+                var role = new IdentityRole();
+                role.Name = "Moderator";
+                role.NormalizedName = "Perform some the operations.";
+                var roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+
+        }
+    }
+}
